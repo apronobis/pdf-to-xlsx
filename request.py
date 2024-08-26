b3b64a6869bc5233904a85baf335c0611e532235
Andrzej Pronobis <a.pronobis1992@gmail.com>
2024-08-27 06:48:20 +0900

    Convert CSV to XLSX (request)

--- request.py
+++ request.py
@@ -1,27 +1,39 @@
 import pdfplumber
-import csv
+import openpyxl
 
-pdf_path = "input/Request of windows.pdf"
-csv_path = "output/request.csv"
+PDF_PATH = "input/Request of windows.pdf"
+XLSX_PATH = "output/request.xlsx"
 
+workbook = openpyxl.Workbook()
+sheet = workbook.active
 
-def filter_none(input_list):
-    return [x for x in input_list if x is not None]
-
-
-with pdfplumber.open(pdf_path) as pdf, open(csv_path, "w") as out:
-    writer = csv.writer(out)
-    writer.writerow(["ID", "Antall", "Bredde (mm)", "Høyde (mm)", "Brannkrav", "Pris"])
+with pdfplumber.open(PDF_PATH) as pdf:
     for page in pdf.pages:
         for table in page.extract_tables():
             while len(table) and table[0][0] != "ID":
                 table.pop(0)
-            if len(table):
-                _id = filter_none(table[0])[1:]
-                antall = filter_none(table[1])[1:]
-                bxh = filter_none(table[3])[1:]
-                b = [item.replace(" ", "").split("×")[0] for item in bxh]
-                h = [item.replace(" ", "").split("×")[-1] for item in bxh]
-                brannkrav = [item or "---" for item in filter_none(table[10])[1:]]
-                for row in zip(_id, antall, b, h, brannkrav):
-                    writer.writerow(row)
+
+            if not len(table):
+                continue
+
+            table[2][0], table[3][0] = "Bredde (mm)", "Høyde (mm)"
+            table = list(zip(*table[:15]))
+
+            if sheet.max_row == 1:
+                sheet.append(table[0])
+
+            for row in table[1:]:
+                if not row[0]:
+                    continue
+
+                row = list(row)
+                row[1] = int(row[1])
+
+                if row[3] == "---":
+                    row[2] = row[3]
+                else:
+                    row[2], row[3] = map(int, row[3].replace(" ", "").split("×"))
+
+                sheet.append(row)
+
+workbook.save(XLSX_PATH)
